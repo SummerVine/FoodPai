91a7e08aab9d0146ebcc34d0c81dd10ca6b4d33a
unknown <green@green-PC.(none)>
2016-07-03 11:57:05 +0800

    Initial commit

---
+++ app/src/main/java/welcome/Welcomeface.java
@@ -0,0 +1,34 @@
+package welcome;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.green.foodpai.MainActivity;
+import com.green.foodpai.R;
+
+/**
+ * Created by green on 2016/7/2.
+ */
+public class Welcomeface extends AppCompatActivity{
+    private ImageView iv_welcome_id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        iv_welcome_id = ((ImageView) findViewById(R.id.iv_welcome_id));
+        new Thread() {
+            @Override
+            public void run() {
+                SystemClock.sleep(5000);
+                Welcomeface.this.startActivity(new Intent(
+                        Welcomeface.this, MainActivity.class));
+                finish();
+            }
+        }.start();
+    }
+
+}
